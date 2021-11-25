51120a20a65cab572fe5e003f15b8b5a2eceafbe
huangwei <huangwei@ke.com>
2021-11-25 16:07:27 +0800

    fix(util) fix warning

--- src/_utils.rs
+++ src/_utils.rs
@@ -26,7 +26,7 @@ pub fn testlog() {
         .build(Root::builder().appender("stdout").build(LevelFilter::Warn))
         .unwrap();
 
-    let handle = log4rs::init_config(config).unwrap();
+    let _handle = log4rs::init_config(config).unwrap();
     
     info!("ceshi info");
     warn!("ceshi info");
@@ -36,71 +36,71 @@ pub fn testlog() {
     // use handle to change logger configuration at runtime
 }
 
-pub fn isAppWebView(userAgent: &str, checkType: &str) -> bool {
-  println!("{}", checkType);
-  match checkType {
+pub fn is_app_webview(user_agent: &str, check_type: &str) -> bool {
+  println!("{}", check_type);
+  match check_type {
     "link" => {
       println!("命中link");
-      return isLink(userAgent)
+      return is_link(user_agent)
     },
     "lianjia" => {
       println!("命中lianjia");
-      return isLianjia(userAgent)
+      return is_lianjia(user_agent)
     },
-    "deyou" => return isDeyou(userAgent),
-    "baichuan" => return isBaichuan(userAgent),
-    "atom" => return isAtom(userAgent),
-    "linkxinfang" => return isLinkXinfang(userAgent),
-    "vrstudio" => return isVRStudio(userAgent),
-    _ => return isLink(userAgent) || 
-      isLianjia(userAgent) || 
-      isDeyou(userAgent) || 
-      isBaichuan(userAgent) || 
-      isAtom(userAgent) || 
-      isLinkXinfang(userAgent) ||
-      isVRStudio(userAgent)
+    "deyou" => return is_deyou(user_agent),
+    "baichuan" => return is_baichuan(user_agent),
+    "atom" => return is_atom(user_agent),
+    "linkxinfang" => return is_link_xinfang(user_agent),
+    "vrstudio" => return is_vr_studio(user_agent),
+    _ => return is_link(user_agent) || 
+      is_link(user_agent) || 
+      is_deyou(user_agent) || 
+      is_baichuan(user_agent) || 
+      is_atom(user_agent) || 
+      is_link_xinfang(user_agent) ||
+      is_vr_studio(user_agent)
   }
 }
 
-fn isLink(userAgent: &str) -> bool {
+fn is_link(user_agent: &str) -> bool {
   let r = Regex::new(r"(?i)Lianjia/(?i)Home(?i)Link/([0-9a-z\.\-]+)").unwrap();
-  return r.is_match(userAgent)
+  return r.is_match(user_agent)
 }
-fn isVRStudio(userAgent: &str) -> bool {
+fn is_vr_studio(user_agent: &str) -> bool {
   let r = Regex::new(r"(?i)V(?i)R(?i)Studio\s*([0-9a-z\.\-]+)").unwrap();
-  return r.is_match(userAgent)
+  return r.is_match(user_agent)
 }
-fn isLinkXinfang(userAgent: &str) -> bool {
+fn is_link_xinfang(user_agent: &str) -> bool {
   let r = Regex::new(r"(?i)Home(?i)Link/([0-9a-z\.\-]+)").unwrap();
-  return r.is_match(userAgent)
+  return r.is_match(user_agent)
 }
-fn isDeyou(userAgent: &str) -> bool {
+fn is_deyou(user_agent: &str) -> bool {
   let r = Regex::new(r"(?i)Lianjia/(?i)Alliance/?([0-9a-z\.\-]+)").unwrap();
   let r1 = Regex::new(r"(?i)Lianjia(?i)Alliance/?([0-9a-z\.\-]+)").unwrap();
-  return r.is_match(userAgent) || r1.is_match(userAgent)
+  return r.is_match(user_agent) || r1.is_match(user_agent)
 }
-fn isBaichuan(userAgent: &str) -> bool {
+fn is_baichuan(user_agent: &str) -> bool {
   let r = Regex::new(r"(?i)Lianjia/(?i)lianjiabaichuan/([0-9a-z\.\-]+)").unwrap();
-  return r.is_match(userAgent)
+  return r.is_match(user_agent)
 }
-fn isBeike(userAgent: &str) -> bool {
+fn is_beike(user_agent: &str) -> bool {
   let r = Regex::new(r"(?i)lianjiabeike/([0-9a-z\.\-]+)").unwrap();
-  return r.is_match(userAgent)
+  return r.is_match(user_agent)
 }
-fn isAtom(userAgent: &str) -> bool {
+fn is_atom(user_agent: &str) -> bool {
   let r = Regex::new(r"(?i)lianjiaatom/([0-9a-z\.\-]+)").unwrap();
-  return r.is_match(userAgent)
+  return r.is_match(user_agent)
 }
-fn isLianjia(userAgent: &str) -> bool {
+fn is_lianjia(user_agent: &str) -> bool {
   let r = Regex::new(r"(?i)Lianjia").unwrap();
-  println!("{}", r.is_match(userAgent));
-  if r.is_match(userAgent) {
+  println!("{}", r.is_match(user_agent));
+  if r.is_match(user_agent) {
     return !(
-      isDeyou(userAgent) || 
-      isBaichuan(userAgent) || 
-      isBeike(userAgent) ||
-      isLink(userAgent) ||
-      isAtom(userAgent)
+      is_deyou(user_agent) || 
+      is_baichuan(user_agent) || 
+      is_beike(user_agent) ||
+      is_link(user_agent) ||
+      is_atom(user_agent)
     )
   }
   return false
@@ -113,7 +113,7 @@ pub fn facilities(n: i32) -> i32 {
   return n * facilities(n - 1);
 }
 
-pub fn appendSearch(uri: &str, search: &str) -> String {
+pub fn append_search(uri: &str, search: &str) -> String {
 
   let mut _uri: Option<&str> = Some(uri);
   if !uri.is_empty() && (uri.ends_with("?") || uri.ends_with("&")) {
